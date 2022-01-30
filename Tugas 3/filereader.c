28771880dda6c92da86f6f2804c243850c6b942a
18320007 Michelle Angelina <98033159+michelleangelinaa@users.noreply.github.com>
2022-01-30 13:46:36 +0700

    Create filereader.c

---
+++ Tugas 3/filereader.c
@@ -0,0 +1,37 @@
+/*
+Nama   : Michelle Angelina
+NIM    : 18320007
+Tanggal: 29 Januari 2022
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    /* KAMUS */
+    FILE *file;
+    char filename[50];
+    char pdfname[54];
+    char jpgname[54];
+
+    printf("Masukkan nama file (tanpa ekstensi/format): ");
+    scanf("%s", filename);
+
+    strcpy(pdfname, filename);
+    strcpy(jpgname, filename);
+    strcat(pdfname, ".pdf");
+    strcat(jpgname, ".jpg");
+
+    if (file = fopen(pdfname, "r")) {
+        printf("File pdf");
+    }
+    else if (file = fopen(jpgname, "r")) {
+        printf("File jpg");
+    }
+    else {
+        printf("File unidentified");
+    }
+
+    return 0;
+}
