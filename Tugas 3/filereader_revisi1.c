07e51f73ab5016aadbe70e11f2283a859f8f3175
18320007 Michelle Angelina <98033159+michelleangelinaa@users.noreply.github.com>
2022-01-30 20:44:01 +0700

    Create filereader_revisi1.c

---
+++ Tugas 3/filereader_revisi1.c
@@ -0,0 +1,14 @@
+/*
+Nama   : Michelle Angelina
+NIM    : 18320007
+Tanggal: 29 Januari 2022
+*/
+
+/*
+Edit 1: 30 Januari 2022, 20:43 WIB
+*/
+
+// Input library
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
